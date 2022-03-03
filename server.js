48b0eec7090c888e15e7f3c8becc9d46e69a8c9c
Fatih Çınar <42092052+thefatihcinar@users.noreply.github.com>
2022-03-04 02:26:59 +0300

    initial trial of making javascript code a web server

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-console.log("Hello World");
+console.log("dandik bir tane appdi");
+
+const express = require('express');
+
+const app = express(); 
+
+function fatihProcessor(request, response){
+    response.send("<h1>fatih </h1>");
+}
+
+function ikinciElKitaplar(request, response){
+    response.send("25");
+}
+
+let ikinciElKitaplarDizisi = []
+ikinciElKitaplarDizisi.push({
+  "id": "1",
+  "name": "Operating System Lecture Notes",
+  "university": "Ankara University"
+});
+
+function kitaplariGetirenFonksiyon(request, response){
+    let kitap = ikinciElKitaplarDizisi[0];
+    kitap.hoca = "özgür hoca";
+
+    // ödeme aldın
+    // mail atar 
+
+    response.send("siparis alındı");
+}
+
+
+app.get("/fatih", fatihProcessor);
+
+app.get("/ikinci-el-kitaplar", kitaplariGetirenFonksiyon);
+
+
+app.listen(5005);
