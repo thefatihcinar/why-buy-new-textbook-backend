18748457d3df79b762422318c4e8a4ca065defea
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-11 15:08:12 +0300

    comment updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import userRoutes from './routes/userRoutes.js'
 
 const app = express(); 
 
-
+// initially we are going to use the static middleware
 // PUT /posts/3/star
 
 app.use("/posts", postRoutes);
