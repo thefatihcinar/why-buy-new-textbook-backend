b7d62e3412cd32551d67598f5fc7d75a68a05028
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-13 02:47:57 +0300

    use colors while connection to mongodb

--- configurations/database.js
+++ configurations/database.js
@@ -12,10 +12,10 @@ const connectDatabase = async () => {
         useNewUrlParser: true
   });
     /* here is means connection is successfully established */
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
   } catch (error) {
     /* if an error occurs while connecting to the database */
-    console.error(`Error while connecting to the MongoDB: ${error.message}`)
+    console.error(`Error while connecting to the MongoDB: ${error.message}`.red.bold.underline)
 
     const FAILURE = 1
     process.exit(FAILURE)
