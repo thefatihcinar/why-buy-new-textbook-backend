41473c65c75426ee30bd0f2329fe1fe08c724367
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-13 03:43:05 +0300

    created post schema

---
+++ models/postModel.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const postSchema = mongoose.Schema(
+  {
+    "title": { type: String, required: true },
+    "price": { type: Number, required: true },
+    "description": { type: String, required: true }
+  },{ timestamps : true});
+
+/* create Post model from this schema */
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
