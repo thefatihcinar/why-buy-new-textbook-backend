39896e9069e123efe29547b097efcd537e1463b3
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-16 02:50:30 +0300

    use not found and error handler middlewares

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import colors from 'colors'
+/* Utilities */
+import connectDatabase from './configurations/database.js'
+/* Routes */
 import postRoutes from './routes/postRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-import connectDatabase from './configurations/database.js'
+/* Middlewares */
+import notFound from './middlewares/notFound.js'
+import errorHandler from './middlewares/errorHandler.js'
 
 /* Configure environment variables */
 dotenv.config();
@@ -17,8 +22,14 @@ const app = express();
 app.use(express.json()); 
 /* JSON body must be in the form of an object */
 
-
+/* Use Routes */
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
+
+/* Not Found Middleware */
+app.use(notFound);
+
+/* Use Custom Error Handler Middleware */
+app.use(errorHandler);
 
 app.listen(5005);
