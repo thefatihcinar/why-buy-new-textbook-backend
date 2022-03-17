51477f1b83803e764a071e401c76f85751618c0d
BenguTercan <62440074+BenguTercan@users.noreply.github.com>
2022-03-17 23:07:53 +0300

    addition of author feature to postModel

--- models/postModel.js
+++ models/postModel.js
@@ -3,12 +3,13 @@ import mongoose from 'mongoose'
 const postSchema = mongoose.Schema(
   {
     "title": { type: String, required: true },
+    "author": { type: String, required: true },
     "price": { type: Number, required: true },
     "description": { type: String, required: true },
     "isApproved": { type: Boolean, required: true, default: false },
     "isDeleted": { type: Boolean, required: true, default: false },
     "mainImage": { type: String, default: "" },
-    "city": { type: String, required: true},
+    "city": { type: String, required: true },
   },{ timestamps : true});
 
 /* create Post model from this schema */
