d8e1650eb18e41439be5ea9d36b4c61e6716336e
BenguTercan <62440074+BenguTercan@users.noreply.github.com>
2022-03-19 11:27:02 +0300

    specify email element unique in user model

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema(
     {
         "name": { type: String, required: true },
         "surname": { type: String, required: true },
-        "email": { type: String, required: true },
+        "email": { type: String, required: true, unique: true },
         "password": { type: String, required: true },
         "schoolName": { type: String },
         "city": { type: String },
