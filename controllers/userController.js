62e355f25d16bf9eeacf83b4eaef7535e106889a
BenguTercan <62440074+BenguTercan@users.noreply.github.com>
2022-03-19 13:54:58 +0300

    implemented draft of user Controller (in progress)

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,8 +9,61 @@ const registerUser = asyncHandler( async (request, response) => {
     // To-Do: Get the user information from json 
     // To-Do: Create user in database
 
-    let registeredUser = await User.create(request.body);
-    response.send(registeredUser)
+    // Get user input
+    let { name, surname, email, password } = req.body;
+
+    // Null check for input
+    if (email === undefined || password === undefined ||name === undefined || surname === undefined) {
+      res.status(400).send("Email, password, name and surname must be provided");
+    }
+
+    // 1. Check if user already exists in the db
+    let existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      return res.status(409).send("User Already Exists. Please Login Again");
+    }
+
+    // 2. User does not exist, Create the user
+
+    // Check whether email is valid or not
+    const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    if(!emailRegexp.test(email)) {
+        response.status(400).send("Email is not in the right format")
+    }
+
+    // Create user in the database
+    let user = await User.create({name,surname,email, password});
+    
+    // Check if user has been created successfully
+    if(user) {
+        response.status(201).send("User is created succesfully")
+        response.json({
+            _id: user._id,
+            name: user.name,
+            surname: user.surname,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+
+//     // Create token
+//     const token = jwt.sign(
+//       { user_id: user._id, email },
+//       process.env.TOKEN_KEY,
+//       {
+//         expiresIn: "2h",
+//       }
+//     );
+//     // save user token
+//     user.token = token;
+
+//     // return new user
+//     res.status(201).json(user);
+//   } catch (err) {
+//     console.log(err);
+//   }
+
 })
 
 const loginUser = asyncHandler( async (request, response) => {
