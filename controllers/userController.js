b1d4b42d3c5b533e631318079373f15e8f4dfcfd
BenguTercan <62440074+BenguTercan@users.noreply.github.com>
2022-03-19 10:55:54 +0300

    use async handler in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,48 +1,49 @@
+import asyncHandler from 'express-async-handler'
 import User from '../models/userModel,js';
 
 // @desc    create new users
 // @route   POST /users
 // @access  private 
-async function registerUser(request, response){
+const registerUser = asyncHandler( async (request, response) => {
 
     // To-Do: Get the user information from json 
     // To-Do: Create user in database
 
     let registeredUser = await User.create(request.body);
     response.send(registeredUser)
-}
+})
 
-function loginUser(request, response){
+const loginUser = asyncHandler( async (request, response) => {
 
     // To-Do: Get the user information from json 
     // To-Do: Get this user from database
     // To-Do: Check user email address and password
 
     response.send("login")
-}
+})
 
-function logoutUser(request, response){
+const logoutUser = asyncHandler( async (request, response) => {
 
     // To-Do: Remove session
 
     response.send("logged out")
-}
+})
 
-function getUserProfile(request, response){
+const getUserProfile = asyncHandler( async (request, response) => {
 
     // To-Do: Learn who connects from token/session
     // To-Do: Get the user from database
 
     response.send("your user profile")
-}
+})
 
-function updateUserProfile(request, response){
+const updateUserProfile = asyncHandler( async (request, response) => {
 
     // To-Do: Get the user information from json 
     // To-Do: Learn who connects from token/session
     // To-Do: Update database
 
     response.send("updated profile")
-}
+})
 
 export {registerUser, loginUser, logoutUser, getUserProfile, updateUserProfile};
