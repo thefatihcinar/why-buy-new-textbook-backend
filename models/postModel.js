4c27c085b5db8c3fa32753c4019d0cd5827ee882
BenguTercan <62440074+BenguTercan@users.noreply.github.com>
2022-03-19 11:00:43 +0300

    edited element property in postModel

--- models/postModel.js
+++ models/postModel.js
@@ -7,7 +7,7 @@ const postSchema = mongoose.Schema(
     "edition": { type: String},
     "language": { type: String, required: true },
     "category": { type: String, required: true },
-    "type": { type: String },
+    "type": { type: String, required: true },
     "price": { type: Number, required: true },
     "condition": { type: String, required: true },
     "description": { type: String, required: true },
