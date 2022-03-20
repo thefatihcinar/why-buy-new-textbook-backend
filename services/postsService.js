f034566d7f095f76436a37cfb5a5fc9e75379b22
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-20 16:47:02 +0300

    implemented create new post service

--- services/postsService.js
+++ services/postsService.js
@@ -7,6 +7,18 @@ import { isEmpty } from '../utilities/emptiness.js'
 
 class PostsService {
 
+  static async createNewPost (post, user){
+    /* this services creates a new post in the database with the given post and user */
+
+    /* Create a new post in the database with the given post data */
+    let createdPost = await Post.create(post); 
+    /* Associate this post with the user who publishes it */
+    await User.updateOne({_id: user._id}, { $push: { publishedPosts: createdPost._id } } );  
+
+    return createdPost;
+
+  }
+
   
 }
 
