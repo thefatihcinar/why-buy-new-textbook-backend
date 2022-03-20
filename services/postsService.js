41d86cd33fbf2a7692a3accf7046a41369d2d70b
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-20 16:46:43 +0300

    created posts service

---
+++ services/postsService.js
@@ -0,0 +1,14 @@
+
+/* Models */
+import Post from "../models/postModel.js";
+import User from '../models/userModel.js'
+/* Utilities */
+import { isEmpty } from '../utilities/emptiness.js'
+
+class PostsService {
+
+  
+}
+
+
+export default PostsService;
