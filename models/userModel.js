9d6029939ff794680409da3de2e2f725b61f1e56
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-20 18:32:18 +0300

    re-implemented user model

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,13 @@ const userSchema = mongoose.Schema(
         "name": { type: String, required: true },
         "email": { type: String, required: true, unique: true },
         "password": { type: String, required: true },
+        "phoneNumber": { type: String, required: true },
+        "profilePicture": { type: String, default: "" },
         "publishedPosts": [ { type: mongoose.Schema.Types.ObjectId, ref: 'Post' } ],
+        "starredPosts": [ { type: mongoose.Schema.Types.ObjectId, ref: 'Post' } ],
+        "recommendedPosts": [ { type: mongoose.Schema.Types.ObjectId, ref: 'Post' } ],
+        "isActive": { type: Boolean, default: true },
+        "isEmailVerified": { type: Boolean, default: false }
     },{ timestamps : true});
 
 /* create User model from this schema */
