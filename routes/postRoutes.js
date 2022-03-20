3a7c26f3cd12820ad16f8b834e20120424906b99
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-20 19:02:30 +0300

    use active middleware in post routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,14 +4,15 @@ import { createPost, updatePost, deletePost, getPost, favoritePost, getRecommend
 /* Middlewares */
 import { authenticate, softAuthentication } from '../middlewares/authentication.js'
 import { postsAuthorization } from '../middlewares/authorization.js'
+import active from '../middlewares/active.js'
 
 const router = express.Router();
 
 router.route("/").get(softAuthentication, getRecommendedPosts);
-router.route("/").post(authenticate, createPost);
-router.route("/:id").put(authenticate, postsAuthorization,  updatePost);
-router.route("/:id").delete(authenticate, postsAuthorization, deletePost);
+router.route("/").post(authenticate, active, createPost);
+router.route("/:id").put(authenticate, active, postsAuthorization, updatePost);
+router.route("/:id").delete(authenticate, active, postsAuthorization, deletePost);
 router.route("/:id").get(getPost);
-router.route("/:id/favorite").put(authenticate, favoritePost);
+router.route("/:id/favorite").put(authenticate, active, favoritePost);
 
 export default router;
