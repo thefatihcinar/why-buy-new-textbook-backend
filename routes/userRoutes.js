3904b65fe537c8b51efd70a93b8e0f56322d4b58
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-20 19:02:38 +0300

    use active middleware for user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,17 @@
 import express from 'express'
 import { registerUser, loginUser, logoutUser, getUserProfile, updateUserProfile } from '../controllers/userController.js'
 
+/* Middlewares */
+import { authenticate, softAuthentication } from '../middlewares/authentication.js'
+import { usersAuthorization } from '../middlewares/authorization.js'
+import active from '../middlewares/active.js'
 
 const router = express.Router();
 
-// post '/users/login' => Hosgeldin Bengu
-
 router.route("/").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/logout").post(logoutUser);
-router.route("/profile").get(getUserProfile);
-router.route("/profile").put(updateUserProfile);
-
+router.route("/logout").post(authenticate, logoutUser);
+router.route("/profile").get(authenticate, active, usersAuthorization, getUserProfile);
+router.route("/profile").put(authenticate, active, usersAuthorization, updateUserProfile);
 
 export default router;
