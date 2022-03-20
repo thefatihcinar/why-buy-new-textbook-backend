098c0e08d2aa51a71c5e0adaab1d335e258593b5
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-20 16:01:38 +0300

    implement publishedPosts feature in users model

--- models/userModel.js
+++ models/userModel.js
@@ -3,12 +3,9 @@ import mongoose from 'mongoose'
 const userSchema = mongoose.Schema(
     {
         "name": { type: String, required: true },
-        "surname": { type: String, required: true },
         "email": { type: String, required: true, unique: true },
         "password": { type: String, required: true },
-        "schoolName": { type: String },
-        "city": { type: String },
-        "phoneNumber": { type: String, required: true },
+        "publishedPosts": [ { type: mongoose.Schema.Types.ObjectId, ref: 'Post' } ],
     },{ timestamps : true});
 
 /* create User model from this schema */
