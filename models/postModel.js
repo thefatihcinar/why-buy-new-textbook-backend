0be2ccf7fad2fb914c7ba7a872a9cc89fe7c57ce
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-20 18:32:12 +0300

    re-implemented post model

--- models/postModel.js
+++ models/postModel.js
@@ -3,19 +3,23 @@ import mongoose from 'mongoose'
 const postSchema = mongoose.Schema(
   {
     "seller": { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    "isShippable": { type: Boolean, default: false },
+    "isAvailableForFacetoFaceSelling": { type: Boolean, default: true },
+    "price": { type: Number, required: true, min: 0 },
     "title": { type: String, required: true },
     "author": { type: String, required: true },
-    "edition": { type: String},
-    "language": { type: String, required: true },
-    "category": { type: String, required: true },
-    "type": { type: String, required: true },
-    "price": { type: Number, required: true },
-    "condition": { type: String, required: true },
-    "description": { type: String, required: true },
-    "isApproved": { type: Boolean, required: true, default: false },
-    "isDeleted": { type: Boolean, required: true, default: false },
     "mainImage": { type: String, default: "" },
-    "city": { type: String, required: true },
+    "relatedCity": { type: mongoose.Schema.Types.ObjectId, ref: 'City', required: true },
+    "relatedInstitution": { type: mongoose.Schema.Types.ObjectId, ref: 'Institution', default: "" },
+    "type": { type: String, required: true },
+    "description": { type: String, required: true },
+    "condition": { type: String, required: true },
+    "isApproved": { type: Boolean, default: true },
+    "isDeleted": { type: Boolean, default: false },
+    "isSold": { type: Boolean, default: false},
+    "images": [ { type: String } ],
+    "starredBy": [ { type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+
   },{ timestamps : true});
 
 /* create Post model from this schema */
