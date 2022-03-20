4b820facc6d084900c14b4f292bf28a593a719ec
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-03-20 16:00:56 +0300

    use authenticatino and authorization middlewares in posts routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,12 +1,16 @@
 import express from 'express'
+/* Controllers */
 import { createPost, updatePost, deletePost, getPost, favoritePost } from '../controllers/postController.js'
+/* Middlewares */
+import authenticate from '../middlewares/authentication.js'
+import { postsAuthorization } from '../middlewares/authorization.js'
 
 const router = express.Router();
 
-router.route("/").post(createPost);
-router.route("/:id").put(updatePost);
-router.route("/:id").delete(deletePost);
+router.route("/").post(authenticate, createPost);
+router.route("/:id").put(authenticate, postsAuthorization,  updatePost);
+router.route("/:id").delete(authenticate, postsAuthorization, deletePost);
 router.route("/:id").get(getPost);
-router.route("/:id/favorite").put(favoritePost);
+router.route("/:id/favorite").put(authenticate, favoritePost);
 
 export default router;
