e11f4168861bae2f6e53e14ade232490e82b5839
Fatih Çınar <42092052+thefatihcinar@users.noreply.github.com>
2022-03-26 18:39:31 +0300

    create messages for user related things

---
+++ messages/userMessages.js
@@ -0,0 +1,10 @@
+
+export default {
+  EMAIL_IS_ALREADY_IN_USE: 'Bu email adresi zaten kullanılıyor.',
+  USER_NOT_FOUND: 'Kullanıcı bulunamadı.',
+  USER_NOT_ACTIVE: 'Kullanıcı deaktif.',
+  USER_NOT_LOGGED_IN: 'Lütfen giriş yapınız.',
+  USER_NOT_AUTHORIZED: 'Yetkisiz Kullanıcı',
+  USER_NOT_FOUND_FOR_THIS_ID: 'Kullanıcı bulunamadı.',
+  USER_NOT_FOUND_FOR_THIS_EMAIL: 'Kullanıcı bulunamadı.',
+};
