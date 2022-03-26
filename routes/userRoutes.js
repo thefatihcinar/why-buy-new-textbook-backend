069194b7bed138423fd57114bf8afa13f2716a95
Fatih Çınar <42092052+thefatihcinar@users.noreply.github.com>
2022-03-26 19:08:16 +0300

    use validators in user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,13 +6,13 @@ import { authenticate, softAuthentication } from '../middlewares/authentication.
 import { usersAuthorization } from '../middlewares/authorization.js'
 import active from '../middlewares/active.js'
 /* Validators */
-import { registerUserValidator, loginUserValidator, updateUserProfileValidator } from '../validators/userValidators.js'
+import { createNewUserValidator, loginUserValidator, updateUserProfileValidator } from '../validators/userValidators.js'
 
 const router = express.Router();
 
-router.route("/").post(registerUser);
-router.route("/login").post(loginUser);
+router.route("/").post(createNewUserValidator, registerUser);
+router.route("/login").post(loginUserValidator, loginUser);
 router.route("/profile").get(authenticate, active, usersAuthorization, getUserProfile);
-router.route("/profile").put(authenticate, active, usersAuthorization, updateUserProfile);
+router.route("/profile").put(authenticate, active, usersAuthorization, updateUserProfileValidator, updateUserProfile);
 
 export default router;
