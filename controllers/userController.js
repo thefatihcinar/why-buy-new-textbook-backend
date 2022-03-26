69f25b838026ca1bc538efd8fcc45f693fb8a387
Fatih Çınar <42092052+thefatihcinar@users.noreply.github.com>
2022-03-26 19:03:27 +0300

    add try catch mechanism in user service, determine the specific error type

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,26 +3,35 @@ import User from '../models/userModel.js';
 /* Services */
 import UsersService from '../services/usersService.js';
 
+
 // @desc    create new users
 // @route   POST /users
 // @access  private 
 const registerUser = asyncHandler( async (request, response) => {
 
-    // To-Do: Get the user information from json 
-    // To-Do: Create user in database
-    let registeredUser = await UsersService.registerNewUser(request.body)
+    try {
+        let registeredUser = await UsersService.registerNewUser(request.body)
 
-    response.send(registeredUser)
+        response.send(registeredUser)
+    } catch (error) {
+        /* If anything goes wrong, determine status code and re-throw the error */
+        response.status(error.code);
+        throw error;
+    }
 })
 
 const loginUser = asyncHandler( async (request, response) => {
 
-    // To-Do: Get the user information from json 
-    // To-Do: Get this user from database
-    // To-Do: Check user email address and password
-    let loggedInUser = await UsersService.loginUser(request.body)
-
-    response.send(loggedInUser)
+    try {
+        let loggedInUser = await UsersService.loginUser(request.body)
+        
+        response.send(loggedInUser)
+    } catch (error) {
+        /* If anything goes wrong, determine status code and re-throw the error */
+        response.status(error.code);
+        throw error;
+    }
+    
 })
 
 const logoutUser = asyncHandler( async (request, response) => {
