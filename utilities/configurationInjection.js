a25b37aa9e41edcb39a1573a594a827c7daebf54
Fatih Çınar <42092052+thefatihcinar@users.noreply.github.com>
2022-03-26 20:33:39 +0300

    create configuration injection utility

---
+++ utilities/configurationInjection.js
@@ -0,0 +1,40 @@
+import * as fs from 'fs'
+
+class ConfigurationInjector {
+  /* this class collects all the configurations and 
+    injects them into the application */
+
+  #configurations = {};
+
+  #readConfigurationFiles(){
+    /* this method will read all the configuration files and 
+      store them in the configurations object */
+    
+    const imageConfigs = JSON.parse( fs.readFileSync('./configurations/imageConfigurations.json', 'utf8'));
+    const appConfigs = JSON.parse( fs.readFileSync('./configurations/applicationConfigurations.json', 'utf8'));
+    
+    /* Store those keys in the configurations object */
+    Object.keys(imageConfigs).forEach(key => {
+      this.#configurations[key] = imageConfigs[key];
+    });
+
+    Object.keys(appConfigs).forEach(key => {
+      this.#configurations[key] = appConfigs[key];
+    });
+
+  }
+  
+  constructor() {
+    /* read all the configuration files store them in an object */
+    this.#readConfigurationFiles();
+  }
+
+  getConfig(key) {
+    /* when a key is provided, 
+       this method finds the necessary configuration in all of the 
+       differen configuration files */
+    return this.#configurations[key];
+  }
+}
+
+export default ConfigurationInjector;
