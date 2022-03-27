41a813e84e7ecc148cd8a413309dcbdc8e3781a0
BenguTercan <62440074+BenguTercan@users.noreply.github.com>
2022-03-27 15:15:44 +0300

    modified and implemented post messages

--- messages/postMessages.js
+++ messages/postMessages.js
@@ -1,4 +1,39 @@
 
 export default {
-  POST_NOT_FOUND: 'İlan bulunamadı.',
+  POST_NOT_CREATED: 'İlan oluşturulamadı.',
+  POST_NOT_FOUND: 'İlan bulunamadı.',
+  
+  TITLE_IS_REQUIRED: 'Başlık girilmesi zorunludur.',
+  TITLE_LENGTH_MINIMUM_MAXIMUM_CHARACTERS: 'Başlık en az 10 en fazla 125 karakterden oluşmalıdır.',
+  TITLE_EMPTY: 'Başlık boş olamaz.',
+
+  AUTHOR_IS_REQUIRED: 'Yazar girilmesi zorunludur.',
+  AUTHOR_LENGTH_MINIMUM_MAXIMUM_CHARACTERS: 'Yazar en az 1 en fazla 200 karakterden oluşmalıdır.',
+  AUTHOR_EMPTY: 'Yazar boş olamaz.',
+
+  CITY_IS_REQUIRED: 'Şehir girilmesi zorunludur.',
+  INVALID_CITY_FORMAT: 'Geçerli bir şehir giriniz.',
+  CITY_EMPTY: 'Şehir boş olamaz.',
+
+  TYPE_IS_REQUIRED: 'Tip girilmesi zorunludur.',
+  INVALID_TYPE_FORMAT: 'Geçerli bir tip giriniz.',
+  TYPE_EMPTY: 'Tip boş olamaz.',
+
+  DESCRIPTION_IS_REQUIRED: 'Açıklama girilmesi zorunludur.',
+  DESCRIPTION_LENGTH_MINIMUM_MAXIMUM_CHARACTERS: 'Açıklama en az 10 en fazla 3000 karakterden oluşmalıdır.',
+  DESCRIPTION_EMPTY: 'Açıklama boş olamaz.',
+
+  CONDITION_IS_REQUIRED: 'Durum girilmesi zorunludur.',
+  INVALID_CONDITION_FORMAT: 'Geçerli bir şehir giriniz.',
+  CONDITION_EMPTY: 'Durum boş olamaz.',
+
+  PRICE_IS_REQUIRED: 'Fiyat girilmesi zorunludur.',
+  PRICE_LENGTH_MINIMUM_MAXIMUM_CHARACTERS: 'Fiyat en az 1 en fazla 10 karakterden oluşmalıdır.',
+  INVALID_PRICE_FORMAT: 'Geçerli bir fiyat giriniz.', 
+  PRICE_MUST_BE_NUMBER: 'Fiyat sayı içermelidir.',
+  PRICE_EMPTY: 'Fiyat boş olamaz.',
+
+
+  SHIPPABLE_TRUE_OR_FALSE: 'Kargolanabilir olup olmadığı belirtilmelidir.',
+  FACETOFACESELLING_TRUE_OR_FALSE: 'Yüz yüze satılıp satılmayacağı belirtilmelidir.',
 };
