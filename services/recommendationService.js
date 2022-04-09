95afbe7a2b069acb83fb04ca4113e4e45d52cba2
Fatih Çınar <42092052+thefatihcinar@users.noreply.github.com>
2022-04-09 14:39:06 +0300

    implementation of associate recommendations with a user service method (complete)

--- services/recommendationService.js
+++ services/recommendationService.js
@@ -1,5 +1,3 @@
-
-
 /* Models */
 import Post from "../models/postModel.js";
 import User from '../models/userModel.js'
@@ -14,7 +12,22 @@ import faker from 'faker';
 
 class RecommendationService {
   
+  static async associateRecommendationsWithUser(searchQueryString, userID){
+    /* whenever a user searches for a post, this service associates the results of the search with the user */
 
+    /* Controls */
+    await UsersServiceHelper.assertUserExists(userID);
+    if( isEmpty(searchQueryString) ) return false;
+
+    const searchResults = await PostsService.searchPost(searchQueryString);
+    /* without any pagination or filtering */
+
+    // Associate the results with the user
+    // in other words, make pre-computation, store all the search result in the user's future recommendations
+    await User.findOneAndUpdate({ _id: userID }, { $addToSet: { recommendedPosts: searchResults } } );
+
+    return true;
+  }
 }
 
 export default RecommendationService;
