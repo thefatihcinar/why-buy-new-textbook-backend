a3c39b69bd9a300cef6129fabe94cf9918c798ae
Fatih Çınar <42092052+thefatihcinar@users.noreply.github.com>
2022-04-09 16:52:17 +0300

    change error throwing mechanism in active middleware

--- middlewares/active.js
+++ middlewares/active.js
@@ -1,4 +1,7 @@
 import asyncHandler from 'express-async-handler';
+import { StatusCodes } from 'http-status-codes';
+/* Messages */
+import msg from '../messages/userMessages.js';
 
 /* Activity Middleware */
 const active = asyncHandler( async (request, response, next) => {
@@ -10,9 +13,10 @@ const active = asyncHandler( async (request, response, next) => {
     }
     else{
       // if this user is an inactive user, do not authorize him
-      
-      response.status(403);
-      throw new Error("Inactive User");
+      const error = new Error();
+        error.message = msg.USER_NOT_ACTIVE;
+        error.code = StatusCodes.FORBIDDEN;
+      throw error;
     }
 
 })
