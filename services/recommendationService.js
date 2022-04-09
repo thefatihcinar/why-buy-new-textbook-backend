ffcbf5c5d49c4a4fe2c9c9f17dbada4f30743c86
Fatih Çınar <42092052+thefatihcinar@users.noreply.github.com>
2022-04-09 14:37:11 +0300

    remove clear user recommendations service

--- services/recommendationService.js
+++ services/recommendationService.js
@@ -5,14 +5,16 @@ import Post from "../models/postModel.js";
 import User from '../models/userModel.js'
 /* Utilities */
 import { isEmpty } from '../utilities/emptiness.js'
+import ConfigurationInjector from '../utilities/configurationInjection.js'
+/* Services */
+import PostsService from "./postsService.js";
+import { UsersServiceHelper } from "./usersService.js";
+
+import faker from 'faker';
 
 class RecommendationService {
   
-  static async recommendedPostsForUser(userID){
-    /* this service brings the recommended posts for a specific user given with user id */
-
-    // TO-DO: Implement this
-  }
+  
 
   static async recommendedPostsBasedOnInstituion(institutionID){
     /* this service brings the recommended posts for a user with the given institution id */
@@ -23,14 +25,6 @@ class RecommendationService {
   static async setFutureRecommendationsForUser(userID, postIDs){
     /* this service gets an array of post ids and a user id and set future recommendations 
        of a user (pre-computation) */
-
-    // TO-DO: Implement this
-  }
-
-  static async clearRecommendationsOfUser(userID){
-    /* 
-       this services clears the pre-computed recommendations of a user given with user id
-    */
 
     // TO-DO: Implement this
   }
