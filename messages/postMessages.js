b294c16eb7e945e01e8980cb77a2ceafdac03b45
Fatih Çınar <42092052+thefatihcinar@users.noreply.github.com>
2022-04-09 17:01:25 +0300

    added new post messages

--- messages/postMessages.js
+++ messages/postMessages.js
@@ -1,5 +1,6 @@
 
 export default {
   POST_NOT_FOUND: 'İlan bulunmadı.',
-  POST_NOT_CREATED: 'İlan oluşturulamadı.'
+  POST_NOT_CREATED: 'İlan oluşturulamadı.',
+  POST_DELETE_CONFIGS_MUST_BE_PROVIDED: 'İlan silme ayarları belirtilmeli.',
 }
